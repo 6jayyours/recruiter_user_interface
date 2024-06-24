ae81bea21d1752092f4b60e0dae36c32c0fbdebd
M ARJUN RAMESHAN <marjunr666@gmail.com>
2024-06-24 11:59:46 +0530

    testtwo

--- src/constants/Url.jsx
+++ src/constants/Url.jsx
@@ -1,5 +1,5 @@
 export const AUTH_URL = "https://recruiterjobs.online/api/v1/auth/"
 
-// export const ADMIN_URL = "https://recruiterjobs.online/api/v1/user/"
+export const ADMIN_URL = "https://recruiterjobs.online/api/v1/user/"
 
 // export const AUTH_URL = "http://localhost:8765/api/v1/auth/"
